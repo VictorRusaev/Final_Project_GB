e3235c142f9778532e6f0c627a64b611d1c1c619
Victor Rusaev <rusaev74@mail.ru>
2022-11-17 19:10:50 +0300

    Make it beautiful

--- Program.cs
+++ Program.cs
@@ -3,24 +3,26 @@
 string[] stringArray = { "hello", "2", "world", ":-)" };
 PrintString(stringArray);
 Console.WriteLine();
+Console.WriteLine("Элементы с длиной меньше или равной трём символам:");
 FindElement(stringArray);
 
 void PrintString(string[] stringArr)
 {
     for (int i = 0; i < stringArr.Length; i++)
-        Console.WriteLine(stringArray[i]);
+        Console.Write($"{stringArray[i]} ");
+        Console.WriteLine();
 }
 
 void FindElement(string[] stringArr)
 {
     int count = 0;
-    string[] finalArr = new string[4];
+    string[] finalArr = new string[stringArr.Length];
     for (int i = 0; i < stringArr.Length; i++)
     {
         if (stringArr[i].Length <= 3)
         {
             finalArr[count] = stringArr[i];
-            Console.WriteLine(finalArr[count]);
+            Console.Write($"{finalArr[count]} ");
             count++;
         }
     }
