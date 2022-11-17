414b68b6d5e9619a4b452074771d946fe6383905
Victor Rusaev <rusaev74@mail.ru>
2022-11-17 06:15:28 +0300

    Input and print strings

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+string[] stringArray = {"hello", "2", "world", ":-)"};
+PrintString(stringArray);
+
+void PrintString (string[] StringArr)
+{
+for (int i = 0; i < stringArray.Length; i++)
+    Console.WriteLine(stringArray[i]);
+}
+
