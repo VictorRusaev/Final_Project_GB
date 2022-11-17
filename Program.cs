c010d49bd59735b6b76fc3a1e1db10e063d4b34e
Victor Rusaev <rusaev74@mail.ru>
2022-11-17 07:11:25 +0300

    Search added. It works

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
 ﻿Console.Clear();
 
-string[] stringArray = {"hello", "2", "world", ":-)"};
+string[] stringArray = { "hello", "2", "world", ":-)" };
 PrintString(stringArray);
+Console.WriteLine();
+FindElement(stringArray);
 
-void PrintString (string[] StringArr)
+void PrintString(string[] stringArr)
 {
-for (int i = 0; i < stringArray.Length; i++)
-    Console.WriteLine(stringArray[i]);
+    for (int i = 0; i < stringArr.Length; i++)
+        Console.WriteLine(stringArray[i]);
+}
+
+void FindElement(string[] stringArr)
+{
+    int count = 0;
+    string[] finalArr = new string[4];
+    for (int i = 0; i < stringArr.Length; i++)
+    {
+        if (stringArr[i].Length <= 3)
+        {
+            finalArr[count] = stringArr[i];
+            Console.WriteLine(finalArr[count]);
+            count++;
+        }
+    }
 }
 
